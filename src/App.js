b2c51c53cbe9e3f422a222a206c837bd62876797
HizemMontassar <montassar.hizem@esprit.tn>
2021-08-05 13:40:26 +0100

    Components part2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristonante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+
+  }
+
+  render(){
+
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristonante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
